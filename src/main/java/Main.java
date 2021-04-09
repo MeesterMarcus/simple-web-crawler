c299bfe8aba57cba5b999cbcb2044912e88108f3
MeesterMarcus <lorenzanamarcus@gmail.com>
2021-04-09 00:07:46 -0500

    error handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,20 @@ import java.io.IOException;
  * immediate domain. I will be using the Jsoup to leverage it's HTML parsing capabilities.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // ensure that url argument was passed
         if (args.length == 1) {
             String url = args[0];
             System.out.println("Crawling "+ url +"...");
             WebCrawler webCrawler = new WebCrawler(url);
-            webCrawler.crawlWebsite();
+            try {
+                webCrawler.crawlWebsite();
+            } catch (IllegalArgumentException e) {
+                System.err.println("Incorrect url was passed: " + url);
+            } catch (IOException e) {
+                System.err.println("There was an error getting url: " + url);
+            }
+
         } else {
             System.err.println("Please pass a URL as command line argument (including http(s)://)");
         }
